ee779cfc4992b0c79cba7eb2ebe78d8aba3a3b26
Louis <colletlouis@gmail.com>
2021-12-02 17:22:24 +0100

    Created Input component.

---
+++ components/NewArgonaut.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="6" lg="4">
+        <h1 class="text-center">Ajouter un·e Argonaute</h1>
+        <v-form class="mt-3 mb-1" cols="8">
+          <v-text-field v-model="name" label="Nom"> </v-text-field>
+        </v-form>
+        <v-row justify="end">
+          <v-btn type="submit" color="primary" class="mr-3 mt-3">Ajouter</v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-app-bar-title class="mt-2 mb-5">
+      <h1 class="text-center">Les Argonautes</h1>
+    </v-app-bar-title>
+    <NewArgonaut />
+  </v-container>
+</template>
+
+<script>
+import NewArgonaut from '../components/NewArgonaut.vue'
+
+export default {
+  components: {
+    NewArgonaut,
+  },
+}
+</script>
+
+<style scoped></style>
