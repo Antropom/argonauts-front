c240bc328ff330eee27b431a85828c645b1d632e
Louis <colletlouis@gmail.com>
2021-12-03 11:11:58 +0100

    Argonaut's list added

---
+++ components/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="argonaut in argonauts"
+        :key="argonaut.id"
+        cols="12"
+        sm="6"
+        md="4"
+      >
+        <v-card-text>
+          <div class="text-center">{{ argonaut.name }}</div>
+        </v-card-text>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      argonauts: (state) => state.argonauts.argonauts,
+    }),
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,18 @@
       <h1 class="text-center">Les Argonautes</h1>
     </v-app-bar-title>
     <NewArgonaut />
+    <List />
   </v-container>
 </template>
 
 <script>
-import NewArgonaut from '../components/NewArgonaut.vue'
+import NewArgonaut from '@/components/NewArgonaut.vue'
+import List from '@/components/List.vue'
 
 export default {
   components: {
     NewArgonaut,
+    List,
   },
 }
 </script>
