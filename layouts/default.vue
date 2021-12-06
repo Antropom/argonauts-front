381fbb850099e52603d807f55046ab549d98b9de
Louis <colletlouis@gmail.com>
2021-12-06 17:04:33 +0100

    Updated meta tags and favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,5 +9,19 @@ export default {
   async fetch() {
     await this.$store.dispatch('argonauts/fetchArgonauts')
   },
+
+  head: {
+    title: 'Les Argonautes',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: "Les membres de l'équipage des Argonautes",
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
 }
 </script>
