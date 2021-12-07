d214975e19b5ee89ae842a8b3c626af2b0d7dd70
Louis <colletlouis@gmail.com>
2021-12-07 14:30:17 +0100

    Added validation
    Added validation

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer color="primary lighten-2" :absolute="true" padless class="mt-10">
+  <v-footer color="primary lighten-2" padless class="mt-10">
     <v-row justify="center" align="center" no-gutters>
       <v-col
         class="primary lighten-2 py-6 text-center white--text"
@@ -27,11 +27,13 @@
           <a
             href="https://github.com/Antropom/argonauts-front"
             class="white--text mr-2"
+            target="blank"
             >Front</a
           >
           <a
             href="https://github.com/Antropom/argonauts-back"
             class="white--text"
+            target="blank"
             >Back</a
           >
         </v-row>
--- components/NewArgonaut.vue
+++ components/NewArgonaut.vue
@@ -4,11 +4,14 @@
       <v-col>
         <h1 class="text-center">Ajouter un·e Argonaute</h1>
         <v-form
+          ref="signUpForm"
+          v-model="formValidity"
           class="mt-3 mb-1 mx-auto"
           style="max-width: 325px"
           @submit.prevent="addArgonaut"
         >
-          <v-text-field v-model="name" label="Nom" required> </v-text-field>
+          <v-text-field v-model="name" label="Nom" required :rules="nameRules">
+          </v-text-field>
           <v-row justify="end">
             <v-btn type="submit" color="primary" class="mr-3 mt-3"
               >Ajouter</v-btn
@@ -25,12 +28,23 @@ export default {
   data() {
     return {
       name: '',
+      nameRules: [
+        (value) =>
+          value.length >= 2 || "Le nom doit être composé d'au moins 2 lettres.",
+        (value) =>
+          value.match(/^[a-zA-Z\s]*$/) || 'Le nom doit être composé de lettres',
+      ],
+      formValidity: false,
     }
   },
   methods: {
     async addArgonaut() {
-      await this.$store.dispatch('argonauts/addArgonaut', this.name)
-      this.name = ''
+      if (this.formValidity) {
+        const argonaut = this.name.trim()
+        await this.$store.dispatch('argonauts/addArgonaut', argonaut)
+        this.name = ''
+        this.$refs.signUpForm.resetValidation()
+      }
     },
   },
 }
